bb73188ce7b9e84028c6f968daf9ff50d3f524e9
Chetan-Marathe <marathecj2004@gmail.com>
2025-07-30 11:28:38 +0530

    second commit

--- autopush.py
+++ autopush.py
@@ -31,11 +31,15 @@ def push_code():
 
     if repo.is_dirty():
         diff = get_git_diff(repo)
+        commit_message = generate_commit_message(diff)
+
+        repo.index.commit(commit_message)
         origin = repo.remote(name='origin')
         origin.push()
-        print("Code pushed")
+        print(f"✅ Code pushed with message: {commit_message}")
     else:
-        print("No changes to commit.")
+        print("📁 No changes to commit.")
+
 
 if __name__ == "__main__":
     push_code()
